eed327f774a2a8feb3f7211ebfb7a3592ff20e5a
Yusuke Matsubara <whym@whym.org>
2015-01-15 19:49:32 +0900

    temporary commit (still not working)

--- ia.py
+++ ia.py
@@ -5,24 +5,47 @@
 
 import sys
 import argparse
+import fileinput
+import codecs
 from collections import namedtuple, defaultdict, Counter
 
-lnode_t = namedtuple('LNode', 'label begin end children')
+larc_t = namedtuple('LArc', 'label begin end children')
 BOS = '<BOS>'
 EOS = '<EOS>'
 
 class Lattice:
     """
-    Lattice is composed with a list of nodes and instance scores.
+    Lattice is composed with a list of arcs and instance scores.
     """
-    def __init__(self, seq):
-        self.lattice = [[lnode_t(BOS, 0, 1, []), 0]]
+    def __init__(self, seq, debug=False):
+        self.debug = debug
+        self.lattice = [{larc_t(BOS, 0, 1, ()): 0}]
         for (i,x) in enumerate(seq):
-            self.lattice.append([lnode_t(x, i+1, i+2, []), 0])
-        self.lattice.append([lnode_t(EOS, len(self.lattice), len(self.lattice)+1, []), 0])
+            self.lattice.append({larc_t(x, i+1, i+2, ()): 0})
+        self.lattice.append({larc_t(EOS, len(self.lattice), len(self.lattice)+1, ()): 0})
+
+    def find_arc(self, label, begin, end):
+        for n in self.lattice:
+            if n.labe == label and n.begin == begin and n.end == end:
+                return n
+        return None
+
+    def find_positions(self, seq):
+        nsize = len(seq)
+        for i in xrange(0, len(self.lattice) - nsize + 1):
+            flag = True
+            for j in xrange(0, nsize):
+                if seq[j] not in [x.label for x in self.lattice[i+j].keys()]:
+                    flag = False
+            if flag:
+                yield i
 
     def add_confirmed_arc(self, label, targets, begin, end):
-        self.lattice.append([lnode_t(label, begin, end, targets), 1])
+        if self.debug:
+            for t in targets:
+                if not self.find_arc(t.label, t.begin, t.end):
+                    raise
+        self.lattice.append([larc_t(label, begin, end, targets), 1])
 
     def __repr__(self):
         return 'Lattice(%s)' % self.lattice
@@ -35,18 +58,21 @@ class Lexicon:
         self.nsize = nsize
         self.counts = counts
         self.scores = scores
-        for i in range(0, len(seq) - nsize + 1):
+        for i in xrange(0, len(seq) - nsize + 1):
             tp = tuple(seq[i:i+nsize])
             self.counts[tp] += 1
             self.scores[tp] = 0
 
     def most_frequent(self):
         for (k,v) in self.counts.most_common():
-            if self.scores[k] == 0:
+            if self.scores[k] == 0: # zero means not presented to the annotator yet
                 yield k
 
     def add_confirmed(self, ngram):
         self.scores[ngram] = 1
+
+    def add_declined(self, ngram):
+        self.scores[ngram] = -1
 
     def __repr__(self):
         return 'Lexicon(nsize=%d, counts=%s, scores=%s)' % (self.nsize, self.counts, self.scores)
@@ -54,18 +80,32 @@ class Lexicon:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-N', '--ngram-size', type=int, default=2,
+    parser.add_argument('-n', '--ngram-size', type=int, default=2,
                         help='N-gram size')
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
                         help='turn on verbose message output')
+    parser.add_argument('ifile')
     options = parser.parse_args()
     if options.verbose:
         print >>sys.stderr, options
 
-    lines = [line.strip() for line in sys.stdin]
+    lines = [line.strip() for line in codecs.open(options.ifile, encoding='utf-8').readlines()]
     lattice = Lattice(lines)
     lexicon = Lexicon(options.ngram_size, lines)
 
     if options.verbose:
         print >>sys.stderr, lattice
         print >>sys.stderr, lexicon
+
+
+    arc_id = 1
+    while True:
+        for k in lexicon.most_frequent():
+            print k
+            p = raw_input('? ')
+            if p.lower()[0] != 'n':
+                lexicon.add_confirmed(k)
+                # TODO: implement # lattice.add_confirmed_arc(arc_id)
+            else:
+                lexicon.add_declined(k)
+            break
--- tests/test_ia.py
+++ tests/test_ia.py
@@ -10,11 +10,11 @@ class TestIA(unittest.TestCase):
 
     def test_cons_lattice(self):
         self.assertEquals(ia.Lattice(x for x in 'abc').lattice,
-                          [[ia.lnode_t(label='<BOS>', begin=0, end=1, children=[]), 0],
-                           [ia.lnode_t(label='a', begin=1, end=2, children=[]), 0],
-                           [ia.lnode_t(label='b', begin=2, end=3, children=[]), 0],
-                           [ia.lnode_t(label='c', begin=3, end=4, children=[]), 0],
-                           [ia.lnode_t(label='<EOS>', begin=4, end=5, children=[]), 0]])
+                          [{ia.larc_t(label='<BOS>', begin=0, end=1, children=()): 0},
+                           {ia.larc_t(label='a', begin=1, end=2, children=()): 0},
+                           {ia.larc_t(label='b', begin=2, end=3, children=()): 0},
+                           {ia.larc_t(label='c', begin=3, end=4, children=()): 0},
+                           {ia.larc_t(label='<EOS>', begin=4, end=5, children=()): 0}])
 
     def test_cons_table(self):
         self.assertEquals(ia.Lexicon(3, [x for x in 'abracadabra']).counts,
